7e6931f2b78d4aa84eea812ac042048e49e113e5
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-06-16 13:32:23 +0000

    wip: calculator app

--- beginners/calculator-app/dist/main.js
+++ beginners/calculator-app/dist/main.js
@@ -5,9 +5,10 @@ const accumulator = document.querySelector("#sec-screen");
 const calcButtonsNumber = document.querySelectorAll("#calc-button-number");
 const clearMainScreenButton = document.querySelector("#clear-main-screen");
 const calcButtonsAll = document.querySelectorAll(".calc-button");
+const calcButtonsOperation = document.querySelectorAll(".operation");
 const MAX_INPUT_LENGTH = 10;
 const total = 0;
-const accumulatorArray = [];
+const accumulatorArray = Array(2).fill(undefined);
 // FUNCTIONS
 const addToTotal = (num) => {
     if (mainScreen.innerHTML.length >= MAX_INPUT_LENGTH)
@@ -22,7 +23,53 @@ const addToTotal = (num) => {
 const clearMainScreen = () => {
     mainScreen.innerHTML = "0";
 };
-const listenToAllOperationsButton = () => { };
+const performOperation = (numA, numB, operation) => {
+    switch (operation) {
+        case "+":
+            return numA + numB;
+        case "-":
+            return numA - numB;
+        case "/":
+            return numA / numB;
+        case "X":
+            return numA * numB;
+        default:
+            // exhausive checks
+            const _exh = operation;
+    }
+};
+const listenToAllOperationsButton = () => {
+    let ppp;
+    calcButtonsOperation.forEach((calButton) => {
+        calButton.addEventListener("click", (e) => {
+            let operation = e.target.innerHTML;
+            console.log(operation);
+            if (!accumulatorArray[0]) {
+                accumulatorArray[0] = parseFloat(mainScreen.innerHTML);
+                mainScreen.innerHTML = "0";
+                console.log("first");
+                accumulator.innerHTML = `${accumulatorArray[0]}`;
+            }
+            else if (!accumulatorArray[1]) {
+                accumulatorArray[1] = parseFloat(mainScreen.innerHTML);
+                ppp = parseFloat(mainScreen.innerHTML);
+                mainScreen.innerHTML = "0";
+                console.log("second");
+                accumulator.innerHTML = `${performOperation(accumulatorArray[0], accumulatorArray[1], operation)}`;
+            }
+            else {
+                console.log("third");
+                const a = parseFloat(accumulator.innerHTML);
+                const b = parseFloat(mainScreen.innerHTML);
+                accumulatorArray[0] = performOperation(a, b, operation);
+                accumulatorArray[1] = undefined;
+                mainScreen.innerHTML = "0";
+                accumulator.innerHTML = `${accumulatorArray[0]}`;
+            }
+            console.log(accumulatorArray);
+        });
+    });
+};
 const listenToAllNumberButtons = () => {
     calcButtonsNumber.forEach((calcButton) => {
         calcButton.addEventListener("click", (e) => {
@@ -51,5 +98,6 @@ const main = () => {
     // events listeners
     addStyleToAllButtonsOnPress();
     listenToAllNumberButtons();
+    listenToAllOperationsButton();
 };
 main();
--- beginners/calculator-app/src/main.ts
+++ beginners/calculator-app/src/main.ts
@@ -4,10 +4,11 @@ const accumulator = document.querySelector("#sec-screen") as HTMLDivElement;
 const calcButtonsNumber = document.querySelectorAll("#calc-button-number");
 const clearMainScreenButton = document.querySelector("#clear-main-screen");
 const calcButtonsAll = document.querySelectorAll(".calc-button");
+const calcButtonsOperation = document.querySelectorAll(".operation");
 const MAX_INPUT_LENGTH = 10;
 const total = 0;
-
-const accumulatorArray: number[] = [];
+const accumulatorArray: Array<number | undefined> = Array(2).fill(undefined);
+type Operation = "/" | "X" | "-" | "+";
 
 // FUNCTIONS
 const addToTotal = (num: string) => {
@@ -26,7 +27,63 @@ const clearMainScreen = () => {
   mainScreen.innerHTML = "0";
 };
 
-const listenToAllOperationsButton = () => {};
+const performOperation = (numA: number, numB: number, operation: Operation) => {
+  switch (operation) {
+    case "+":
+      return numA + numB;
+    case "-":
+      return numA - numB;
+    case "/":
+      return numA / numB;
+    case "X":
+      return numA * numB;
+    default:
+      // exhausive checks
+      const _exh: never = operation;
+  }
+};
+
+const listenToAllOperationsButton = () => {
+  let ppp: number;
+  calcButtonsOperation.forEach((calButton) => {
+    calButton.addEventListener("click", (e) => {
+      let operation: string = (e.target as HTMLDivElement).innerHTML;
+      console.log(operation);
+
+      if (!accumulatorArray[0]) {
+        accumulatorArray[0] = parseFloat(mainScreen.innerHTML);
+        mainScreen.innerHTML = "0";
+        console.log("first");
+
+        accumulator.innerHTML = `${accumulatorArray[0]}`;
+      } else if (!accumulatorArray[1]) {
+        accumulatorArray[1] = parseFloat(mainScreen.innerHTML);
+        ppp = parseFloat(mainScreen.innerHTML);
+        mainScreen.innerHTML = "0";
+        console.log("second");
+
+        accumulator.innerHTML = `${performOperation(
+          accumulatorArray[0],
+          accumulatorArray[1],
+          operation as Operation
+        )}`;
+      } else {
+        console.log("third");
+        const a = parseFloat(accumulator.innerHTML);
+        const b = parseFloat(mainScreen.innerHTML);
+
+        accumulatorArray[0] = performOperation(a, b, operation as Operation);
+
+        accumulatorArray[1] = undefined;
+        mainScreen.innerHTML = "0";
+
+        accumulator.innerHTML = `${accumulatorArray[0]}`;
+      }
+
+      console.log(accumulatorArray);
+    });
+  });
+};
 
 const listenToAllNumberButtons = () => {
   calcButtonsNumber.forEach((calcButton) => {
@@ -59,6 +116,7 @@ const main = () => {
   // events listeners
   addStyleToAllButtonsOnPress();
   listenToAllNumberButtons();
+  listenToAllOperationsButton();
 };
 
 main();
