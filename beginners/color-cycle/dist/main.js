2864ca1a71cc903299a727e7a80624a50fa9d3ed
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-06-18 22:13:43 +0000

    wip

--- beginners/color-cycle/dist/main.js
+++ beginners/color-cycle/dist/main.js
@@ -13,12 +13,16 @@ const isHexa = (str) => {
     }
     return false;
 };
+// generate a random number between 0 - 255(16) to avoid repeating color pattern
+const generateRandomNumberBetween0And255 = () => {
+    return Math.floor(Math.random() * 256);
+};
 // function to increment the hex figure by a number
 const incrementHexa = (current, incremental) => {
     const newNum = current + incremental;
-    if (newNum <= 255 || newNum >= 0)
+    if (newNum <= 255 && newNum >= 0)
         return newNum;
-    return (newNum - 255).toString(16);
+    return generateRandomNumberBetween0And255().toString(16);
 };
 startButton.addEventListener("click", () => {
     let redValue = redInput.value;
@@ -26,12 +30,13 @@ startButton.addEventListener("click", () => {
     let blueValue = blueInput.value;
     if (isHexa(redValue) && isHexa(greenValue) && isHexa(blueValue)) {
         colorCode = redValue + greenValue + blueValue;
+        document.body.style.background = "#" + colorCode;
         setInterval(() => {
             redValue = incrementHexa(parseInt(redValue, 16), Number(incrementalValue.value) || 20).toString(16);
             greenValue = incrementHexa(parseInt(greenValue, 16), Number(incrementalValue.value) || 20).toString(16);
             blueValue = incrementHexa(parseInt(blueValue, 16), Number(incrementalValue.value) || 20).toString(16);
             colorCode = redValue + greenValue + blueValue;
-            console.log(colorCode);
+            document.body.style.background = "#" + colorCode;
         }, 250);
     }
 });
--- beginners/color-cycle/src/main.ts
+++ beginners/color-cycle/src/main.ts
@@ -1,48 +1,65 @@
-const startButton = document.getElementById("start-button") as HTMLButtonElement
-const redInput = document.getElementById("red") as HTMLInputElement
-const greenInput = document.getElementById("green") as HTMLInputElement
-const blueInput = document.getElementById("blue") as HTMLInputElement
-const incrementalValue = document.getElementById("increment") as HTMLInputElement
+const startButton = document.getElementById(
+  "start-button"
+) as HTMLButtonElement;
+const redInput = document.getElementById("red") as HTMLInputElement;
+const greenInput = document.getElementById("green") as HTMLInputElement;
+const blueInput = document.getElementById("blue") as HTMLInputElement;
+const incrementalValue = document.getElementById(
+  "increment"
+) as HTMLInputElement;
 
-let colorCode: string
+let colorCode: string;
 
 // confirm that number can be converted to hexaecimal and is less than 255
 const isHexa = (str: string) => {
-  const number = parseInt(str, 16)
+  const number = parseInt(str, 16);
 
   if (number <= 255 || number >= 0) {
-    return true
+    return true;
   }
-  return false
-}
+  return false;
+};
 
+// generate a random number between 0 - 255(16) to avoid repeating color pattern
+const generateRandomNumberBetween0And255 = () => {
+  return Math.floor(Math.random() * 256);
+};
 
 // function to increment the hex figure by a number
 const incrementHexa = (current: number, incremental: number) => {
-  const newNum = current + incremental
+  const newNum = current + incremental;
 
-  if (newNum <= 255 || newNum >= 0) return newNum
-  return (newNum - 255).toString(16)
-}
+  if (newNum <= 255 && newNum >= 0) return newNum;
+  return generateRandomNumberBetween0And255().toString(16);
+};
 
 startButton.addEventListener("click", () => {
-  let redValue = redInput.value 
-  let greenValue = greenInput.value
-  let blueValue = blueInput.value
+  let redValue = redInput.value;
+  let greenValue = greenInput.value;
+  let blueValue = blueInput.value;
 
   if (isHexa(redValue) && isHexa(greenValue) && isHexa(blueValue)) {
-    colorCode = redValue + greenValue + blueValue 
+    colorCode = redValue + greenValue + blueValue;
+
+    document.body.style.background = "#" + colorCode;
 
     setInterval(() => {
-      redValue = incrementHexa(parseInt(redValue, 16), Number(incrementalValue.value) || 20).toString(16)
-      greenValue = incrementHexa(parseInt(greenValue, 16), Number(incrementalValue.value) || 20).toString(16)
-      blueValue = incrementHexa(parseInt(blueValue, 16), Number(incrementalValue.value) || 20).toString(16)
+      redValue = incrementHexa(
+        parseInt(redValue, 16),
+        Number(incrementalValue.value) || 20
+      ).toString(16);
+      greenValue = incrementHexa(
+        parseInt(greenValue, 16),
+        Number(incrementalValue.value) || 20
+      ).toString(16);
+      blueValue = incrementHexa(
+        parseInt(blueValue, 16),
+        Number(incrementalValue.value) || 20
+      ).toString(16);
 
-      colorCode = redValue+greenValue+blueValue
+      colorCode = redValue + greenValue + blueValue;
 
-      console.log(colorCode)
-
-    }, 250)
+      document.body.style.background = "#" + colorCode;
+    }, 250);
   }
-})
-
+});
